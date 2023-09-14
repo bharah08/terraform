c3fa325f7cf1cb8eb948df37cca3ce17d34f19a8
bharah08 <111727649+bharah08@users.noreply.github.com>
2023-09-14 17:11:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,11 @@
-provider "aws" {
-    region = "ap-south-1"  # Set your desired AWS region
-}
 
-resource "aws_instance" "example" {
-    ami           = "ami-0f5ee92e2d63afc18"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+resource "aws_instance" "ec2_example" {
+
+   ami           = var.ami_type   ### ami type from var.tf
+   instance_type =  var.instance_type #### instance type from var.tf
+   count         = var.instance_count #### instance count from tf
+
+   tags = {
+           Name = "Terraform EC2"
+   }
 }
