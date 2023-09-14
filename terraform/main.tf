b57ab7522e8d450ef6b1514daf7f334c3651a6de
bharah08 <111727649+bharah08@users.noreply.github.com>
2023-09-14 19:32:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,14 @@ resource "aws_instance" "ec2_example" {
    ami           = var.ami_type   ### ami type from var.tf
    instance_type =  var.instance_type #### instance type from var.tf
    count         = var.instance_count #### instance count from tf
-
+   associate_public_ip_address = var.enable_public_ip
    tags = {
            Name = "Terraform EC2"
    }
 }
+
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
+
