0dfccd88dae4bbf398ae8973f52b46cfee1b039a
bharah08 <111727649+bharah08@users.noreply.github.com>
2023-09-15 12:15:19 +0530

    Create main.tf

---
+++ terraform/localvariables/main.tf
@@ -0,0 +1,41 @@
+##### 2. Benefits of using Terraform Locals
+If you are working on a large enterprise infrastructure then it is impossible to work without Terraform Locals. But let's see the benefits of using terraform locals in a bit more detail -
+
+Terraform locals can be re-used multiple numbers of times in the terraform configuration file.
+It can reduce the work of updating your terraform configuration at multiple places. With terraform locals you need to update its value once and it should reflect all over the place where it is referred.
+
+3. Create your first Terraform Local on AWS
+Let's create our first Terraform local and in this terraform local we will store the Tag name for the AWS EC2 instance.
+-------------------------------
+locals {
+  staging_env = "staging"
+}
+
+resource "aws_vpc" "staging-vpc" {
+  cidr_block = "10.5.0.0/16"
+
+  tags = {
+    Name = "${local.staging_env}-vpc-tag"
+  }
+}
+
+resource "aws_subnet" "staging-subnet" {
+  vpc_id = aws_vpc.staging-vpc.id
+  cidr_block = "10.5.0.0/16"
+
+  tags = {
+    Name = "${local.staging_env}-subnet-tag"
+  }
+}
+
+resource "aws_instance" "ec2_example" {
+   
+   ami           = "ami-0767046d1677be5a0"
+   instance_type = "t2.micro"
+   subnet_id = aws_subnet.staging-subnet.id
+   
+   tags = {
+           Name = "${local.staging_env} - Terraform EC2"
+   }
+}
+BASH
